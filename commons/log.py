6bfe03bd73622454d4b3fa5bd804f0d4ba339d49
kurashns <nikitaqrush@gmail.com>
2024-05-14 18:42:43 +0600

    commit

--- commons/log.py
+++ commons/log.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 def _get_file_handler(level: int | str = INFO) -> FileHandler:
-    logs_filename = f'log/gibdd_scraper_{datetime.now().strftime(format="%Y-%m-%d_%H-%M-%S")}.log'
+    logs_filename = f'logs/gibdd_scraper_{datetime.now().strftime(format="%Y-%m-%d_%H-%M-%S")}.log'
     file_handler = FileHandler(filename=logs_filename, encoding='utf-8')
 
     file_handler.setLevel(level)
